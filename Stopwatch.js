787425f2c587f5740f26c9c4baa01f454346e2ee
Bartek <bartekma89@gmail.com>
2018-02-12 17:26:43 +0100

    added result list

--- Stopwatch.js
+++ Stopwatch.js
@@ -5,8 +5,18 @@ class Stopwatch extends React.Component {
 			running: false,
 			minutes: 0,
 			seconds: 0,
-			miliseconds: 0
+			miliseconds: 0,
+			resultList: [],
+			id: 1
 		}
+	}
+
+	reset() {
+		this.setState({
+			minutes: 0,
+			seconds: 0,
+			miliseconds: 0	
+		})
 	}
 
 	format(times) {
@@ -19,11 +29,11 @@ class Stopwatch extends React.Component {
 		})
 		if(this.state.miliseconds >= 100) {
 			this.setState( (prevState) => {return {seconds: prevState.seconds + 1}});
-			this.setState( () => {return {miliseconds: 0}});
+			this.setState( {miliseconds: 0});
 		}
 		if(this.state.seconds >= 60) {
 			this.setState( (prevState) => {return {minutes: prevState.minutes + 1}});
-			this.setState( () => { return {seconds: 0}});
+			this.setState( {seconds: 0});
 		}
 	}
 
@@ -52,16 +62,46 @@ class Stopwatch extends React.Component {
 		return (result.length < 2) ? '0' + result : result;
 	}
 
+	shotTime() {
+		console.log('shot');
+			this.setState( (prevState) => { return { id: prevState.id + 1 }} )
+			let onceTime = {
+				time: this.format(this.state),
+				id: this.state.id
+			};
+
+			console.log(onceTime.time);
+			console.log(this.state.id);
+
+			this.setState( (prevState) => { return {
+				resultList: [onceTime, ...prevState.resultList]
+			}})
+
+			console.log(this.state.resultList);
+	}
+
 	render() {
 		return(
 				<div id="container">
 					<div className="controls">
-						<Button text="Start" onClick={this.start.bind(this)}/>
-						<Button text="Stop"  onClick={this.stop.bind(this)}/>
-						<Button text="Reset" />
+						<Button text="Start" onClick={this.start.bind(this)} />
+						<Button text="Stop"  onClick={this.stop.bind(this)} />
+						<Button text="Reset" onClick={this.reset.bind(this)} />
+						<Button text="Save" onClick={this.shotTime.bind(this)} />
+						<Button text="Reset List" />
 					</div>
 					<div className="stopwatch">{this.format(this.state)}</div>
-					<ul className="result">Result list</ul>
+					<ul className="result">
+						{
+							this.state.resultList.map( item => {
+
+								return (
+										<li key={item.id}>{item.time}</li>
+									)
+
+							})
+						}
+					</ul>
 				</div>
 			)
 	}
@@ -71,4 +111,4 @@ const Button = (props) => {
 	return <button type='button' className="btn" {...props}>{props.text}</button>
 }
 
-ReactDOM.render(<Stopwatch />, document.getElementById('root'));
+ReactDOM.render(<Stopwatch />, document.getElementById('root')); 
